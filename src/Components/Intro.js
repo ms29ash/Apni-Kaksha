6c6f60caa8b6baa034b9d9bf52e57831652b6d13
ms29ash <ms29ash@gmail.com>
2022-06-15 00:10:23 +0530

    add link  in intro

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -10,7 +10,7 @@ function Intro() {
         <Container id="a">
             <Details data-aos="fade-down-right">
                 <Head>Premium <br /> Education for All</Head>
-                <Button>Take a tour<Icon /></Button>
+                <Button href="https://www.youtube.com/watch?v=aDH1cy51Oc0" target="_blank">Take a tour<Icon /></Button>
             </Details>
             <Wrapper data-aos="fade-down-left">
                 <Img src="/images/IntroSection.svg" alt="" />
@@ -24,7 +24,7 @@ export default Intro
 const Container = tw.div`flex items-center w-screen px-32 justify-center h-screen`
 const Details = tw.div`h-screen w-[50%] flex justify-center items-start flex-col p-6`
 const Head = tw.h1`text-6xl whitespace-normal	font-semibold`
-const Button = tw.button` flex items-center border-payment-purple border-2 rounded-md px-5 text-payment-purple text-lg my-4 py-4 hover:underline transition-all hover:scale-105 mt-16`
+const Button = tw.a` cursor-pointer flex items-center border-payment-purple border-2 rounded-md px-5 text-payment-purple text-lg my-4 py-4 hover:underline transition-all hover:scale-105 mt-16`
 const Icon = tw(BsPlayCircle)`ml-2 text-xl`
 const Wrapper = tw.div``
 const Img = tw.img`h-screen p-4`
