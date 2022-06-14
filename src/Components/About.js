6092b7ce171b4f5a2b678813979d0980768585bf
ms29ash <ms29ash@gmail.com>
2022-06-14 07:49:36 +0530

    fix leading in about

--- src/Components/About.js
+++ src/Components/About.js
@@ -28,6 +28,6 @@ const Section = tw.section`bg-white min-h-screen p-32`
 const Container = tw.div`bg-cream flex rounded-xl  p-24`
 const Wrapper = tw.div`mx-12 items-baseline px-10 w-6/12`
 const Box = tw.div`w-6/12`
-const Head = tw.h1`text-4xl font-semibold leading-6`
+const Head = tw.h1`text-4xl font-semibold leading-8`
 const SubHead = tw.p`text-2xl text-black/80 mt-6`
 const Img = tw.img`w-8/12 object-cover`
