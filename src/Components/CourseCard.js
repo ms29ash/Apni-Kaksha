78020e31de1bed77efa5660fe028240116396589
ms29ash <ms29ash@gmail.com>
2022-07-11 17:55:51 +0530

    Fix Responsiveness of Course

--- src/Components/CourseCard.js
+++ src/Components/CourseCard.js
@@ -9,31 +9,34 @@ function CourseCard(props) {
     AOS.init();
     return (
 
-        <Container data-aos={`fade-${props.type}`}>
-            <Head>{props.title}</Head>
-            <Ul>
-                {
-                    props.points.map((point, index) => {
+        <Container
+            data-aos={`fade-${props.type}`}
+        >
+            <Wrapper>
 
-                        return <LI key={index}><Icon /> <Text>{point}</Text></LI>
-                    })
-                }
+                <Head>{props.title}</Head>
+                <Ul>
+                    {
+                        props.points.map((point, index) => {
 
-            </Ul>
-            <Button>Learn More</Button>
+                            return <LI key={index}><Icon /> <Text>{point}</Text></LI>
+                        })
+                    }
+
+                </Ul>
+                <Button>Learn More</Button>
+            </Wrapper>
         </Container>
-
     )
 }
 
 export default CourseCard
 
-
-
-const Container = tw.div` shadow-xl p-12 mt-16  rounded-xl  xl:w-5/12 h-[110vh] w-full max-h-[900px]`
-const Head = tw.h3`text-4xl font-semibold mb-10 px-4 w-full leading-[2.75rem]`
-const Ul = tw.ul``
+const Container = tw.div` py-[10%] flex xl:w-[45%] w-[90%] m-4  shadow-xl rounded-xl items-center justify-center`
+const Wrapper = tw.div` w-[70%]  `
+const Head = tw.h3`lg:text-4xl text-2xl font-semibold mb-10 px-4 w-full leading-[2.75rem]`
+const Ul = tw.ul`flex-[1_0_auto]`
 const LI = tw.li`text-left text-lg flex justify-start items-baseline pb-3 text-black/80 w-full `
 const Icon = tw(BsFillCheckCircleFill)`text-payment-purple text-xl  w-2/12`
 const Text = tw.p`w-10/12`
-const Button = tw.button`border-black/80 w-full rounded-md py-6 mt-16 border-2 text-payment-purple hover:bg-payment-purple hover:text-white`
+const Button = tw.button`border-black/80 w-full rounded-md lg:py-8 py-4  mt-16 border-2 text-payment-purple hover:bg-payment-purple hover:text-white`
